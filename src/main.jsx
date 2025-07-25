f4be37f75f5b6c9970662af8c1e9c5be7625b3e4
Ganzina Daniil <energion03@gmail.com>
2025-07-25 20:22:37 +0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
       <Toaster
         position="top-right"
@@ -19,6 +19,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           },
         }}
       />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 ) 
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Myrna-Ugoda/', // Замените на имя вашего репозитория
   server: {
     port: 3000,
     open: true
